e5d3e78ae5dffb4e8baf9ecc210f49a24ddefb6c
MarianoCinalli <mariano_cinalli@hotmail.com>
2017-10-05 18:02:12 -0300

    Cleaning main

--- TP1/src/main.cpp
+++ TP1/src/main.cpp
@@ -9,20 +9,14 @@ int LOG_MIN_LEVEL = 1;
 std::ofstream LOG_FILE_POINTER;
 // Constants ------------------------------------------------------
 
-void dosomething() { 
-    log("Hola mi pid es ", getpid(), 3);
-}
-
 int main(int argc, char* argv[]) {
     // Initialization
-    srand(time(NULL)); // Para calcular randoms
-    std::string logFileName = "beachVoley.log";
-    LOG_FILE_POINTER.open(logFileName, std::ofstream::app);
+    srand(time(NULL)); // Init seed for random
+    LOG_FILE_POINTER.open("beachVoley.log", std::ofstream::app);
     // End Initialization
     logSessionStarted();
     // Main body
     // End Main body
 	logSessionFinished();
-
 	return 0;
 }
