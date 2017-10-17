8bc88b60a9a2139a52c3ad171cc19589ebf51137
ruben <rbnm.jimenez@gmail.com>
2017-10-17 15:23:30 -0300

    comment include semaforo

--- TP1/headers/courtManager/CourtManager.h
+++ TP1/headers/courtManager/CourtManager.h
@@ -8,7 +8,7 @@
 #include "tools/Messages.h"
 #include "fifos/FifoLectura.h"
 #include "fifos/FifoEscritura.h"
-#include "semaforos/Semaforo.h"
+//#include "semaforos/Semaforo.h"
 #include "courtManager/CourtCM.h"
 #include "courtManager/MessageCM.h"
 
