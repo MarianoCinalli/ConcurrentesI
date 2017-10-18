51448fb09c30491ac57abd0caafa7d0692615d93
MarianoCinalli <mariano_cinalli@hotmail.com>
2017-10-17 23:03:46 -0300

    Fixing log and fifo for closing

--- TP1/src/courtManager/CourtManager.cpp
+++ TP1/src/courtManager/CourtManager.cpp
@@ -32,7 +32,7 @@ void CourtManager::initializeCourts() {
 };
 
 void CourtManager::administrateCourts() {
-	log("El court manager esta administrando las canchas", 3);
+	log("CourtManager: El court manager esta administrando las canchas", 3);
 	bool close = false;
 	MessageCM* readMessage;
 	while(!close) {
@@ -40,18 +40,18 @@ void CourtManager::administrateCourts() {
 		close = this->processMessage(readMessage);
 		delete(readMessage);
 	}
-	log("Cerrando court manager", 3);
+	log("CourtManager: Cerrando court manager", 3);
 };
 
 MessageCM* CourtManager::getMessage() {
 	struct messageCourtManager readMessage;
 	int readBytes = this->fifoCourtManager->leer(static_cast<void*>(&readMessage), sizeof(readMessage));
 	if (readBytes < 0) {
-		log("** Error ** En la lectura. Errno: ", strerror(errno), 1);
+		log("CourtManager: ** Error ** En la lectura. Errno: ", strerror(errno), 1);
 	} else if (readBytes == 0) {
-		log("** Warning ** Se leyeron 0 bytes. Ignorando.", 2);
+		log("CourtManager: ** Warning ** Se leyeron 0 bytes. Ignorando.", 2);
 	} else {
-		log("Mensaje leido correctamente. Cantidad de bytes: ", readBytes, 3);
+		log("CourtManager: Mensaje leido correctamente. Cantidad de bytes: ", readBytes, 3);
 		return (new MessageCM(readMessage));
 	}
 	return (new MessageCM());
@@ -60,20 +60,20 @@ MessageCM* CourtManager::getMessage() {
 bool CourtManager::processMessage(MessageCM* message) {
 	bool shouldEnd = false;
 	int operation = message->getOperation();
-	log("Se recibio un mensaje. Operacion: ", operation, 3);
+	log("CourtManager: Se recibio un mensaje. Operacion: ", operation, 3);
 	if (operation == raiseType) {
 		log("Se recibio el mensaje de incremento de marea.", 3);
 		this->raiseTideAndInformCourts(message->getOperationValue());
 		shouldEnd = false;
 	} else if (operation == lowType) {
-		log("Se recibio el mensaje de decremento de marea.", 3);
+		log("CourtManager: Se recibio el mensaje de decremento de marea.", 3);
 		this->lowerTideAndInformCourts(message->getOperationValue());
 		shouldEnd = false;
 	} else if (operation == closeCourts) {
-		log("Se recibio el mensaje de cerrado de cancha.", 3);
+		log("CourtManager: Se recibio el mensaje de cerrado de cancha.", 3);
 		shouldEnd = true;
 	} else {
-		log("Warning ** Se ignora el mensaje.", 2);
+		log("CourtManager: Warning ** Se ignora el mensaje.", 2);
 		shouldEnd = false;
 	}
 	return shouldEnd;
@@ -81,36 +81,36 @@ bool CourtManager::processMessage(MessageCM* message) {
 
 void CourtManager::raiseTideAndInformCourts(int amountToRaise) {
 	int previousTideLevel = this->tideLevel;
-	log("Aumentando marea " + std::to_string(previousTideLevel) + " en " + std::to_string(amountToRaise), 3);
+	log("CourtManager: Aumentando marea " + std::to_string(previousTideLevel) + " en " + std::to_string(amountToRaise), 3);
 	// Actualizo el valor de la marea (o es mayor o es igual al anterior)
 	this->tideLevel = this->getUpdatedTideLevel(amountToRaise);
 	if (previousTideLevel < this->tideLevel) {
-		log("Aumentando la marea.", 2);
+		log("CourtManager: Aumentando la marea.", 2);
 		for (int rowNumber = previousTideLevel; rowNumber < this->tideLevel; ++rowNumber) {
 			for (int columnNumber = 0; columnNumber < this->columns; ++columnNumber) {
 				this->beach[rowNumber][columnNumber]->closeCourt();
 			}
 		}
 	} else {
-		log("Warning ** El valor para actualizar la marea es incorrecto. Ignorando.", 2);
+		log("CourtManager: Warning ** El valor para actualizar la marea es incorrecto. Ignorando.", 2);
 	}
 };
 
 void CourtManager::lowerTideAndInformCourts(int amountToLower) {
 	int previousTideLevel = this->tideLevel;
-	log("Disminuyendo marea " + std::to_string(previousTideLevel) + " en " + std::to_string(amountToLower), 3);
+	log("CourtManager: Disminuyendo marea " + std::to_string(previousTideLevel) + " en " + std::to_string(amountToLower), 3);
 	// Actualizo el valor de la marea (o es menor o es igual al anterior)
 	this->tideLevel = this->getUpdatedTideLevel((-1) * amountToLower);
 	if (previousTideLevel > this->tideLevel) {
 		// Voy desde la cancha que estaba indundada hasta la 
-		log("Disminuyendo marea.", 3);
+		log("CourtManager: Disminuyendo marea.", 3);
 		for (int rowNumber = previousTideLevel; rowNumber > this->tideLevel; --rowNumber) {
 			for (int columnNumber = 0; columnNumber < this->columns; ++columnNumber) {
 				this->beach[rowNumber][columnNumber]->openCourt();
 			}
 		}
 	} else {
-		log("Warning ** El valor para disminuir la marea produce un resultado negativo. Ignorando.", 2);
+		log("CourtManager: Warning ** El valor para disminuir la marea produce un resultado negativo. Ignorando.", 2);
 	}
 };
 
@@ -126,12 +126,9 @@ int CourtManager::getUpdatedTideLevel(int amountToAdd) {
 void CourtManager::shutDownCourts() {
 	log("CourtManager: Abriendo fifo para cerrar las canchas", 3);
 	FifoEscritura* fifoMatches = new FifoEscritura("/tmp/fifoMatches");
+	fifoMatches->abrir();
 	log("CourtManager: Fifo abierto", 3);
-	struct messageMatch closeMessage;
-	closeMessage.idPlayer1_Team1 = 1;
-	closeMessage.idPlayer2_team1 = 1;
-	closeMessage.idPlayer1_Team2 = 1;
-	closeMessage.idPlayer2_team2 = 1;
+	struct messageMatch closeMessage = (const struct messageMatch){ CLOSE };
 	log("CourtManager: Cerrando " + std::to_string(this->rows * this->columns) + " canchas.", 3);
 	for (int rowNumber = 0; rowNumber < this->rows; ++rowNumber) {
 		for (int columnNumber = 0; columnNumber < this->columns; ++columnNumber) {
