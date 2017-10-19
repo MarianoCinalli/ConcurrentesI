d3988ea12ba56beb9f9d674a18ce906c5e2f83d1
MarianoCinalli <mariano_cinalli@hotmail.com>
2017-10-19 18:31:19 -0300

    Adding fifo initializer

---
+++ TP1/headers/fifos/FifoInitializer.h
@@ -0,0 +1,18 @@
+#ifndef FIFOINITIALIZER_H_
+#define FIFOINITIALIZER_H_
+
+#include "tools/Constants.h"
+#include "fifos/FifoLectura.h"
+#include <vector>
+
+class FifoInitializer {
+
+	private:
+		std::vector<FifoLectura*>* fifos;
+
+	public:
+		void createFifos();
+		void deleteFifos();
+};
+
+#endif // FIFOINITIALIZER_H_
---
+++ TP1/src/fifos/FifoInitializer.cpp
@@ -0,0 +1,15 @@
+#include "fifos/FifoInitializer.h"
+
+void FifoInitializer::createFifos() {
+	this->fifos = new std::vector<FifoLectura*>();
+	this->fifos->push_back(new FifoLectura(FIFO_WRITE_COMMAND_TO_PLAYERMANAGER));
+	this->fifos->push_back(new FifoLectura(FIFO_WRITE_COMMAND_TO_COURTMANAGER));
+	this->fifos->push_back(new FifoLectura(FIFO_WRITE_PLAYER_TO_TEAMMANAGER));
+	this->fifos->push_back(new FifoLectura(FIFO_WRITE_TEAM_TO_MATCHMANAGER));
+	this->fifos->push_back(new FifoLectura(FIFO_WRITE_MATCH_TO_MATCHES));
+	this->fifos->push_back(new FifoLectura(FIFO_WRITE_RESULT_TO_RESULTMANAGER));
+}
+
+void FifoInitializer::deleteFifos() {
+
+}
