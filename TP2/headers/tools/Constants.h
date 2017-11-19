07c6b991bccf76882e937224f808c4393786c20f
MarianoCinalli <mariano_cinalli@hotmail.com>
2017-11-18 22:09:55 -0300

    Updating services messages

--- TP2/headers/tools/Constants.h
+++ TP2/headers/tools/Constants.h
@@ -1,7 +1,23 @@
 #ifndef CONSTANTS_H_
 #define CONSTANTS_H_
 
-enum logLevels{ERROR = 1, WARNING = 2, INFORMATION = 3};
-enum servicesErrorsIds{NO_ERROR = 0, NOT_FOUND = 1};
+enum logLevels {
+	ERROR = 1,
+	WARNING = 2,
+	INFORMATION = 3
+};
+
+// Para los errores en las operaciones de los servicios.
+enum servicesErrorsIds {
+	NO_ERROR = 0,
+	NOT_FOUND = 1
+};
+
+// Tipos de operaciones soportadas por servicios.
+enum servicesOperations {
+	SERVICE_OP_CREATE = 0,
+	SERVICE_OP_UPDATE = 1,
+	SERVICE_OP_READ = 2
+};
 
 #endif // CONSTANTS_H_
--- TP2/headers/tools/Messages.h
+++ TP2/headers/tools/Messages.h
@@ -3,18 +3,34 @@
 
 #include "Constants.h"
 
+	// Mensajes para Altas, modificaciones y consultas. ----------------------------
+	// Por ahora consideremos que los clientes (el administrador tambien) 
+	// conocen las ids de los servicios (por mas que por ahora no la mostremos).
+	// operationType = SERVICE_OP_CREATE o SERVICE_OP_UPDATE o SERVICE_OP_READ.
+	// En caso de un SERVICE_OP_CREATE se ignora la id (porque se le va a asignar una nueva
+	// con los valores de newTemperature, newPressure, newHumidity).
+	// En caso de un SERVICE_OP_READ newTemperature, newPressure, newHumidity se ignoran.
+	// En caso de un SERVICE_OP_UPDATE se usan id, newTemperature, newPressure, newHumidity.
+	// Si una id no esta en la lista del servicio en el mensaje de respuesta se devuelve errorId = NOT_FOUND
 	struct messageRequestWeatherService {
 		long mtype;
 		long replyTo;
+		int operationType;
 		int cityId;
+		int newTemperature;
+		int newPressure;
+		int newHumidity;
 	};
 
 	struct messageRequestExchangeRatesService {
 		long mtype;
 		long replyTo;
+		int operationType;
 		int currencyId;
+		int newExchangeRate;
 	};
 
+	// Mensajes de respuesta -------------------------------------------------------
 	// [Temperature] = Celsius - [Pressure] = ATM  - [Humidity] = %
 	struct messageReplyWeatherService {
 		long mtype;
